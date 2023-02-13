f10679da0bfe60856677d914788176214b26aa6f
Araceli Galiano Salas <araceligalianosalas@gmail.com>
2023-02-13 18:47:15 +0100

    Iteración 3

--- routes/drones.js
+++ routes/drones.js
@@ -19,12 +19,25 @@ router.get('/drones', async (req, res, next) => {
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  res.render("drones/create-form.hbs")
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post('/drones/create', async (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
-});
+  try {
+    const newDrone = req.body
+    const response = await DroneModel.create({
+      name: newDrone.name,
+      propellers: newDrone.propellers,
+      maxSpeed: newDrone.maxSpeed
+    })
+
+res.redirect("/drones")
+
+  } catch(err) {
+    next(err)
+  }
+ });
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
